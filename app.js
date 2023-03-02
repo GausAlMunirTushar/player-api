8debefeccf25b3d5d030aa2b5c0eca7d0e67fdaa
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-02 17:32:19 +0600

    player create, get and update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const shortid = require('shortid')
+const fs = require('fs/promises')
+const path = require('path')
+const dbLocation = path.resolve('src', './db.json');
 const app = express();
 
 app.use(cors());
@@ -18,6 +21,60 @@ app.use(express.json());
 * PATCH - /:id  - update player
 * DELETE- /:id  - delete  a player from db
 */
+
+// Find Player by ID
+app.patch('/:id', async (req, res) => {
+    const id = req.params.id
+    const data = await fs.readFile(dbLocation)
+    const players = JSON.parse(data)
+    const player = players.find((item) => item.id === id )
+    if(!player){
+        return res.status(404).json({
+            message : 'Player Not Found'
+        })
+    }
+    player.name = req.body.name || player.name
+    player.country = req.body.country || player.country
+    player.rank == req.body.rank || player.rank
+    
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(201).json(player)
+})
+
+// Update Player
+
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const data = await fs.readFile(dbLocation)
+    const players = JSON.parse(data)
+    const player = players.find((item) => item.id === id )
+    if(!player){
+        return res.status(404).json({
+            message : 'Player Not Found'
+        })
+    }
+    res.status(201).json(player)
+})
+
+app.post('/', async (req, res)=>{
+    const player = {
+        ...req.body,
+        id: shortid.generate()
+    };
+    const data = await fs.readFile(dbLocation)
+    const players = JSON.parse(data)
+    players.push(player);
+
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(201).json(player)
+
+})
+app.get('/', async (req, res) => {
+    const data = await fs.readFile(dbLocation)
+    const players = JSON.parse(data)
+    res.status(201).json(players)
+})
+    
 app.get('/health', (req, res)=>{
     res.status(200).json({
         status: 'OK',
